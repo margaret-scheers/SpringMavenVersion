24e55b94665c162f0194eb6d766129cf3ed3206b
margaret-scheers <margaret.scheers@pobox.com>
2021-03-11 17:26:01 -0600

    mix xml and component scan bean definitions

--- 01-spring-in-depth/src/main/java/com/in28minutes/spring/basics/springin5steps/SpringIn5StepsXMLContextApplication.java
+++ 01-spring-in-depth/src/main/java/com/in28minutes/spring/basics/springin5steps/SpringIn5StepsXMLContextApplication.java
@@ -2,19 +2,23 @@ package com.in28minutes.spring.basics.springin5steps;
 
 import com.in28minutes.spring.basics.springin5steps.basic.BinarySearchImpl;
 import com.in28minutes.spring.basics.springin5steps.xml.XmlPersonDAO;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-
+@ComponentScan
 public class SpringIn5StepsXMLContextApplication {
+	private static Logger LOGGER = LoggerFactory.getLogger(SpringIn5StepsXMLContextApplication.class);
 
 	public static void main(String[] args) {
 
 		try (ClassPathXmlApplicationContext applicationContext =
 					 new ClassPathXmlApplicationContext("applicationContext.xml")) {
 
+			LOGGER.info("Beans --> {}",(Object) applicationContext.getBeanDefinitionNames());
 			XmlPersonDAO xmlPersonDAO =
 					applicationContext.getBean(XmlPersonDAO.class);
 
