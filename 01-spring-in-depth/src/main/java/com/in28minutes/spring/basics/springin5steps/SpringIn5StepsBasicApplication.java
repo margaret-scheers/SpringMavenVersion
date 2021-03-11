2b896afb40d039ba35fc2ec0b03b315f5d2d7c85
margaret-scheers <margaret.scheers@pobox.com>
2021-03-11 13:38:25 -0600

    update other apps

--- 01-spring-in-depth/src/main/java/com/in28minutes/spring/basics/springin5steps/SpringIn5StepsBasicApplication.java
+++ 01-spring-in-depth/src/main/java/com/in28minutes/spring/basics/springin5steps/SpringIn5StepsBasicApplication.java
@@ -1,21 +1,18 @@
 package com.in28minutes.spring.basics.springin5steps;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 import com.in28minutes.spring.basics.springin5steps.basic.BinarySearchImpl;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SpringIn5StepsBasicApplication {
 
-	// What are the beans?
-	// What are the dependencies of a bean?
-	// Where to search for beans? => No need
-
 	public static void main(String[] args) {
 
-		ApplicationContext applicationContext = 
+		ApplicationContext applicationContext =
 				new AnnotationConfigApplicationContext(SpringIn5StepsBasicApplication.class);
 		
 		BinarySearchImpl binarySearch = 
--- 01-spring-in-depth/src/main/java/com/in28minutes/spring/basics/springin5steps/basic/BinarySearchImpl.java
+++ 01-spring-in-depth/src/main/java/com/in28minutes/spring/basics/springin5steps/basic/BinarySearchImpl.java
@@ -1,12 +1,21 @@
 package com.in28minutes.spring.basics.springin5steps.basic;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope(ConfigurableBeanFactory.SCOPE_SINGLETON)
 public class BinarySearchImpl {
+	
+	private Logger logger = LoggerFactory.getLogger(this.getClass()); 
 
 	@Autowired
+	@Qualifier("bubble")
 	private SortAlgorithm sortAlgorithm;
 	
 	public int binarySearch(int[] numbers, int numberToSearchFor) {
@@ -16,6 +25,4 @@ public class BinarySearchImpl {
 		// Search the array
 		return 3;
 	}
-
-
 }
