74aebd5fe5d3db90fdb699c8a8c78cddce8dcc80
margaret-scheers <margaret.scheers@pobox.com>
2021-03-11 14:25:53 -0600

    move steps to folder

--- 01-spring-in-depth/src/main/java/com/in28minutes/spring/basics/springin5steps/SpringIn5StepsBasicApplication.java
+++ 01-spring-in-depth/src/main/java/com/in28minutes/spring/basics/springin5steps/SpringIn5StepsBasicApplication.java
@@ -13,20 +13,24 @@ public class SpringIn5StepsBasicApplication {
 
 	public static void main(String[] args) {
 
-		AnnotationConfigApplicationContext applicationContext =
-				new AnnotationConfigApplicationContext(SpringIn5StepsBasicApplication.class);
-		
-		BinarySearchImpl binarySearch = 
-				applicationContext.getBean(BinarySearchImpl.class);
+		try (AnnotationConfigApplicationContext applicationContext =
+					 new AnnotationConfigApplicationContext(SpringIn5StepsBasicApplication.class)) {
 
-		BinarySearchImpl binarySearch1 = 
-				applicationContext.getBean(BinarySearchImpl.class);
+			BinarySearchImpl binarySearch =
+					applicationContext.getBean(BinarySearchImpl.class);
 
-		System.out.println(binarySearch);
-		System.out.println(binarySearch1);
-		
-		int result = 
-				binarySearch.binarySearch(new int[] { 12, 4, 6 }, 3);
-		System.out.println(result);
+			BinarySearchImpl binarySearch1 =
+					applicationContext.getBean(BinarySearchImpl.class);
+
+			System.out.println(binarySearch);
+			System.out.println(binarySearch1);
+
+			int result =
+					binarySearch.binarySearch(new int[]{12, 4, 6}, 3);
+			System.out.println(result);
+			applicationContext.close();
+
+
+		}
 	}
 }
